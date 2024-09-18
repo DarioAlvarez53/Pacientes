dab2fce6a6956f49051b4add5bf616a3b2e6c087
Dario Alvarez <daralv53@gmail.com>
2024-09-18 16:56:33 -0600

    Trabajando en el listado de pacientes

--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -1,12 +1,40 @@
 import { usePatientStore } from "../store/store"
+import PatientDetail from "./PatientDetail"
 
 export default function PatientList() {
 
-    const patients = usePatientStore()
-
-    console.log(patients);
+    const patients = usePatientStore((state) => state.patients)
     
     return (
-        <div>PatientList</div>
+        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+            {patients.length ? (
+                <>
+                    <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Aministra tus{''}
+                        <span className="text-indigo-600 font-bold">
+                            Pacientes y Citas
+                        </span>
+                        {/* En caso de que si tenga pacientes comenzara a iterar */}
+                        {patients.map(patient => (
+                            <PatientDetail 
+                                key={patient.id}
+                                patient={patient.name}
+                            />
+                        ))}
+                    </p>
+                </>
+            ) : (
+                <>
+                    <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                    Comienza agregando pacientes{''}
+                        <span className="text-indigo-600 font-bold">
+                            y apareceran en este lugar
+                        </span>
+                    </p>
+                </>
+            )}
+        </div>
     )
 }
