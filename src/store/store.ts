83228430d7956ce74629cddeabb7383d33e2b4ef
Dario Alvarez <daralv53@gmail.com>
2024-09-18 16:32:17 -0600

    Escribiendo en el estado de zustand

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,7 @@
 //Aqui van a vivir el state y las acciones de manera global
 //La forma de crear un store es importando "create"
 import { create } from "zustand"
+import { v4 as uuidv4} from 'uuid'
 import { DraftPatient, Patient } from "../types"
 
 type PatientState = {
@@ -8,11 +9,17 @@ type PatientState = {
     addPatient: (data: DraftPatient) => void
 }
 
+const createPatient = (patient: DraftPatient) : Patient => {
+    return { ...patient, id: uuidv4()}
+}
+
 //Aqui se va colocar tanto el state como las funciones que modificaran al state
-export const usePatientStore = create<PatientState>(() => ({
+export const usePatientStore = create<PatientState>((set) => ({  /* aqui se pueden pasar un par de funciones (set, get) set es para ageregar un valor, y get es para recibir un valor */
     patients: [],
     addPatient: (data) => {
-        console.log(data);
-        
+        const newPatient = createPatient(data)
+        set((state) => ({
+            patients: [...state.patients, newPatient]
+        }))
     }
 }))
